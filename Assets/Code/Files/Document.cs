2e7b826c24485402367ad4d515397828e51d70cc
auusi9 <ausi_dalmau@hotmail.com>
2021-01-31 11:21:00 +0100

    Removed target ID from document

--- Assets/Code/Files/Document.cs
+++ Assets/Code/Files/Document.cs
@@ -5,10 +5,7 @@ namespace Code.Files
     public class Document : MonoBehaviour
     {
         [SerializeField] private PanelElement _panelElement;
-        [SerializeField] private string _targetId;
-
-        public string TargetId => _targetId;
-
+        
         public void Configure(Canvas canvas, RectTransform parent)
         {
             _panelElement.Configure(canvas, parent);
