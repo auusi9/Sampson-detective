75ffea19926164795008a233887e80be87ec71ff
auusi9 <ausi_dalmau@hotmail.com>
2021-01-31 11:18:12 +0100

    Buttons

---
+++ Assets/Code/Map/DisableObjectOnClick.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Code.Map
+{
+    public class DisableObjectOnClick : MonoBehaviour
+    {
+        [SerializeField] private GameObject _targetGameObject;
+        [SerializeField] private Button _button;
+
+        private void Start()
+        {
+            _button.onClick.AddListener(ToggleGameobject);
+        }
+
+        private void ToggleGameobject()
+        {
+            _targetGameObject.gameObject.SetActive(!_targetGameObject.activeSelf);
+        }
+        
+        private void OnDestroy()
+        {
+            _button.onClick.RemoveListener(ToggleGameobject);
+        }
+    }
+}
