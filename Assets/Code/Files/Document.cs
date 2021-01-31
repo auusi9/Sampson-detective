d9cba6206b2dce3f4f12bef767dd7a4af78c415d
auusi9 <ausi_dalmau@hotmail.com>
2021-01-31 10:45:02 +0100

    Document target id

--- Assets/Code/Files/Document.cs
+++ Assets/Code/Files/Document.cs
@@ -5,6 +5,10 @@ namespace Code.Files
     public class Document : MonoBehaviour
     {
         [SerializeField] private PanelElement _panelElement;
+        [SerializeField] private string _targetId;
+
+        public string TargetId => _targetId;
+
         public void Configure(Canvas canvas, RectTransform parent)
         {
             _panelElement.Configure(canvas, parent);
