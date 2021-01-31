4fce71d570f4caee9c29ac5aa8886ddf64798e1b
auusi9 <ausi_dalmau@hotmail.com>
2021-01-31 17:29:33 +0100

    MainMenu

---
+++ Assets/Code/Utils/OpenSceneButton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Code.Utils
+{
+    public class OpenSceneButton : MonoBehaviour
+    {
+        [SerializeField] private int _scene;
+        [SerializeField] private Button _button;
+
+        private void Start()
+        {
+            _button.onClick.AddListener(OpenScene);
+
+            if (PlayerPrefs.GetInt("LevelMax", 1) < _scene)
+            {
+                _button.interactable = false;
+            }
+        }
+
+        private void OnDestroy()
+        {
+            _button.onClick.RemoveListener(OpenScene);
+        }
+
+        public void OpenScene()
+        {
+            SceneManager.LoadScene(_scene);
+        }
+    }
+}
